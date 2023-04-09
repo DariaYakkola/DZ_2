00d104a47482b8b221a40a0bc6e1ffe2ce3eb35e
Daria Yakkola <yakkolabest@gmail.com>
2023-04-09 23:25:15 +0300

    Исправила опечатку

--- Task_2.py
+++ Task_2.py
@@ -1,5 +1,5 @@
-number = int (input('Введите трехзначное число: '))
+number = int(input('Введите трехзначное число: '))
 handred = number // 100
 ten = (number % 100) // 10
 one = (number % 100) % 10
-print (f'Cумма чисел равна - {handred + ten + one}')
+print(f'Cумма чисел равна - {handred + ten + one}')
