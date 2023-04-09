214437e6164712b14f1e8b310c07ed521c91cd94
Daria Yakkola <yakkolabest@gmail.com>
2023-04-09 23:23:44 +0300

    Задача 2

---
+++ Task_2.py
@@ -0,0 +1,5 @@
+number = int (input('Введите трехзначное число: '))
+handred = number // 100
+ten = (number % 100) // 10
+one = (number % 100) % 10
+print (f'Cумма чисел равна - {handred + ten + one}')
